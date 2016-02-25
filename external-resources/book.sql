b1119c1d31e271a46096f33b8a68b754f8ee36b4
ravi nair <rnair@mobiquityinc.com>
2016-02-25 14:42:31 +0530

    Refactored

--- external-resources/book.sql
+++ external-resources/book.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `book`
 --
-
+create database if not exists oauth2;
 DROP TABLE IF EXISTS `book`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
